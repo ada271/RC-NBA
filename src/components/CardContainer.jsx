cdb3e04b99c44819fb6cb51e5215f331deb11cca
ada <h.kaansolmaz@gmail.com>
2024-10-01 22:48:12 +0300

    mbr

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
+
 import {data} from "../helper/data"
+import { Col, Container, Row } from 'react-bootstrap'
+import Player from './Player'
 
 const CardContainer = () => {
     // console.log(data)
   return (
     <>
-        
+        <Container>
+            <Row>
+                {data.map((player)=>(
+                    <Col key={player.name}>
+                        <Player/>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
 
     </>
   )
