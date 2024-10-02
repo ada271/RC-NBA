517f4781dbf7480cc07a39832fc8a1af3c63bcfd
ada <h.kaansolmaz@gmail.com>
2024-10-02 16:02:35 +0300

    statistics

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
 
-const Player = ({name,img,statistics}) => {
-  
-  return (
-    <Card className='rounded-2 m-auto player-kard' role='button'>
-      <Card.Img className='player-foto' variant="top" src={img} />
-      <Card.Footer >
-        <Card.Title>{name}</Card.Title>
-      </Card.Footer>
-    </Card>
-  )
-}
+const Player = ({ name, img, statistics }) => {
+    const [showResim, setShow] = useState(true);
 
-export default Player
+    return (
+        <Card
+            onClick={() => setShow(!showResim)}
+            className="rounded-2 m-auto player-kard"
+            role="button"
+        >
+            {showResim ? (
+                <Card.Img className="player-foto" variant="top" src={img} />
+            ) : (
+                <ul>
+                    {statistics.map((item,index) => (
+                        🏀<li key={index} >{item}</li>
+                    ))}
+                </ul>
+            )}
+
+            <Card.Footer>
+                <Card.Title>{name}</Card.Title>
+            </Card.Footer>
+        </Card>
+    );
+};
+
+export default Player;
