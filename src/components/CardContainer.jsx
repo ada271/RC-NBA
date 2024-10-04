6c58a681dce681447b49f0698bd4748e74a965ee
ada <h.kaansolmaz@gmail.com>
2024-10-04 13:40:35 +0300

    st

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 
 import {data} from "../helper/data"
-import { Col, Container, Row } from 'react-bootstrap'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import Player from './Player'
 
 const CardContainer = () => {
     // console.log(data)
   return (
     <>
+        
+        <Form.Control className='w-50 m-auto' type="search" placeholder="Search Player..." />
+
         <Container className='player-container my-4 rounded-4 p-3'>
             <Row className='g-3 justify-content-center'>
                 {data.map((player)=>(
